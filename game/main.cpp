405742364729eadd096c5bac9875c0bed8da81e9
SCBenson <sean_c_benson@outlook.com>
2026-08-23 19:03:51 +0100

    implemented a fixed-timestep loop for 60fps frame rate.

--- game/main.cpp
+++ game/main.cpp
@@ -17,6 +17,13 @@ int main(int argc, char* argv[]) {
 
 	
 	bool running = true;
+	const double dt = 1.0 / 60.0;
+	double accumulator = 0.0;
+	double simTime = 0.0;
+	Uint64 lastTicks = SDL_GetTicks();
+
+	float red = 0.0f, green = 0.0f, blue = 0.0f;
+	float prevRed = 0.0f, prevGreen = 0.0f, prevBlue = 0.0f;
 
 	while (running) {
 		SDL_Event event;
@@ -25,14 +32,34 @@ int main(int argc, char* argv[]) {
 				running = false;
 		}
 
-		const double now = ((double)SDL_GetTicks()) / 1000.0;
-		const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-		const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-		const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-		SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+		const Uint64 currentTicks = SDL_GetTicks();
+		double frameTime = (currentTicks - lastTicks) / 1000.0;
+		lastTicks = currentTicks;
 
+		if (frameTime > 0.25) frameTime = 0.25;
+
+		accumulator += frameTime;
+
+		while (accumulator >= dt) {
+			prevRed = red;
+			prevGreen = green;
+			prevBlue = blue;
+
+			simTime += dt;
+			red = (float)(0.5 + 0.5 * SDL_sin(simTime));
+			green = (float)(0.5 + 0.5 * SDL_sin(simTime + SDL_PI_D * 2 / 3));
+			blue = (float)(0.5 + 0.5 * SDL_sin(simTime + SDL_PI_D * 4 / 3));
+
+			accumulator -= dt;
+		}
+
+		const double alpha = accumulator / dt;
+		const float r = (float)(prevRed + (red - prevRed) * alpha);
+		const float g = (float)(prevGreen + (green - prevGreen) * alpha);
+		const float b = (float)(prevBlue + (blue - prevBlue) * alpha);
+
+		SDL_SetRenderDrawColorFloat(renderer, r, g, b, SDL_ALPHA_OPAQUE_FLOAT);
 		SDL_RenderClear(renderer);
-
 		SDL_RenderPresent(renderer);
 	}
 
